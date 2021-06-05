80af98e89c55f07dc881077c96f79c9e41fad04a
da_silva_405 <senna.da.silva.405@gmail.com>
2021-06-05 12:57:11 +0900

    新規ポートフォリオ

---
+++ js/formCheck.js
@@ -0,0 +1,24 @@
+const $form = document.getElementById('form');
+const $submit = document.getElementById('submit');
+
+// 親要素(form)にイベントをつけると子要素のイベントが拾える
+$form.addEventListener('change', update);
+$form.addEventListener('input', update);
+
+// デフォルトではEnterキー押下でも送信されてしまうのでsubmitイベントで制御する
+$form.addEventListener('submit', () => {
+	//e.preventDefault();
+	$form.submit();
+});
+
+function update(e) {
+  // バリデーションが総合的に通っているかどうかのフラグ
+  const isValid = $form.checkValidity();	//checkValidity→ input要素の入力内容の検証（boolean型）
+
+  if (isValid) {  // 通っていれば活性化
+    $submit.removeAttribute('disabled');
+    return;
+  }
+  // 通っていなければ非活性にする
+  $submit.setAttribute('disabled', 'disabled');
+}
